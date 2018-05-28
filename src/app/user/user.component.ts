0b5bf10872bbe3a6b6cf67fc7e7a3bd026541d0e
synergy2411 <synergy2411@outlook.com>
2018-05-28 13:01:06 +0530

    property binding added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
     selector : 'app-user',
-    template : '<h1>User Component Loaded!!</h1>',
+    //template : '<h1>{{ user.firstName }} {{ user.lastName }}</h1>',
+    templateUrl : './user.component.html',
     styles : [`
         h1{
             color : blue;
         }
     `]
 })
-export class UserComponent{}
+export class UserComponent{
+    user  = {
+        firstName : "Bill",
+        lastName : "Gates",
+        dob : new Date("Dec 1, 1963"),
+        income : 50000,
+        company : "Microsoft",
+        isWorking : true
+    }
+}
